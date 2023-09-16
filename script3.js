a8bebf2bc4cdfd23060259fa1f559810c1263eff
Toffa04 <145034154+Toffa04@users.noreply.github.com>
2023-09-16 17:50:48 +0200

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,9 +1,9 @@
 // Dati di esempio delle squadre
 const squadre = [
-    { posizione: 1, nome: "Betis Siviglia", punti: 0},
-    { posizione: 2, nome: "Chelsea", punti: 0 },
+    { posizione: 1, nome: "Betis Siviglia", punti: 3},
+    { posizione: 4, nome: "Chelsea", punti: 0 },
     { posizione: 3, nome: "Milan", punti: 0 },
-    { posizione: 4, nome: "Hellas Verona", punti: 0},
+    { posizione: 2, nome: "Hellas Verona", punti: 3},
     { posizione: 5, nome: "Juventus", punti: 0},
     { posizione: 6, nome: "Brescia", punti: 0},
     { posizione: 7, nome: "Montorio", punti: 0}
