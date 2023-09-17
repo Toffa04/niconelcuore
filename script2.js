8dde1a94ec3545896eb743b9a02fc700cbaae9df
Toffa04 <145034154+Toffa04@users.noreply.github.com>
2023-09-17 10:24:04 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,12 +1,12 @@
 // Dati di esempio delle squadre
 const squadre2 = [
-    { posizione: 5, nome: "BP Stoccolma", punti: 4 },
+    { posizione: 3, nome: "BP Stoccolma", punti: 4 },
     { posizione: 6, nome: "Sarnico", punti: 3 },
     { posizione: 7, nome: "Inter", punti: 2 },
     { posizione: 2, nome: "Atletico Madrid", punti: 12},
-    { posizione: 3, nome: "Atalanta", punti: 4},
+    { posizione: 4, nome: "Atalanta", punti: 4},
     { posizione: 1, nome: "Polisportiva virtus", punti: 15},
-    { posizione: 4, nome: "Benfica", punti: 4}
+    { posizione: 5, nome: "Benfica", punti: 4}
     // Aggiungi più squadre secondo necessità
 ];
 
